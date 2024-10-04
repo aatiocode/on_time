ff2d08346451bfe0003b52bd3637fa933e6dc517
Aristio Rizki Pratama Putra <macbook@NB-P93292-2.local>
2024-10-04 11:04:50 +0700

    add floating button

--- lib/presentation/feature/home/home_screen.dart
+++ lib/presentation/feature/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:floating_action_bubble/floating_action_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:on_time/presentation/feature/note/note_screen.dart';
 import 'package:on_time/presentation/feature/setting/setting_screen.dart';
@@ -12,10 +13,13 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   List<(IconData icon, String label, Widget page)> menu = [];
 
   int selectedIndex = 0;
+  late Animation<double> _animation;
+  late AnimationController _animationController;
 
   void onItemTapped(int index) {
     setState(() {
@@ -25,30 +29,71 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    super.initState();
     menu = [
       (Icons.calendar_month, "Schedule", ScheduleScreen()),
       (Icons.note, "Note", NoteScreen()),
       (Icons.settings, "Setting", SettingScreen()),
     ];
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation =
+        CurvedAnimation(curve: Curves.easeInOut, parent: _animationController);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: menu[selectedIndex].$3,
-      bottomNavigationBar: CustomBottomNav(
-        menu: menu,
-        onTap: onItemTapped,
-        currentIndex: selectedIndex,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFF7E64FF),
-        onPressed: () {
-          // Navigator.pushNamed(context, Routes.ATTENDANCE);
-        },
-        child: const Icon(Icons.add, color: Colors.white),
-      ),
-    );
+        body: menu[selectedIndex].$3,
+        bottomNavigationBar: CustomBottomNav(
+          menu: menu,
+          onTap: onItemTapped,
+          currentIndex: selectedIndex,
+        ),
+        floatingActionButton: FloatingActionBubble(
+          // Menu items
+          items: <Bubble>[
+            // Floating action menu item
+            Bubble(
+              title: "Schedule",
+              iconColor: Colors.white,
+              bubbleColor: const Color(0xFF7E64FF),
+              icon: Icons.edit_calendar,
+              titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+              onPress: () {
+                _animationController.reverse();
+              },
+            ),
+            // Floating action menu item
+            Bubble(
+              title: "Note",
+              iconColor: Colors.white,
+              bubbleColor: const Color(0xFF7E64FF),
+              icon: Icons.note_add,
+              titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+              onPress: () {
+                _animationController.reverse();
+              },
+            ),
+          ],
+          // animation controller
+          animation: _animation,
+          // On pressed change animation state
+          onPress: () => _animationController.isCompleted
+              ? _animationController.reverse()
+              : _animationController.forward(),
+
+          // Floating Action button Icon color
+          iconColor: Colors.white,
+          // Flaoting Action button Icon
+          iconData: Icons.add,
+          backGroundColor: const Color(0xFF7E64FF),
+        ));
   }
 }
